b830cb58aa920f064b93833aa3725c6bde4670c5
Szpachlarz <kl.kolakowska@gmail.com>
2023-04-03 04:38:39 +0200

    support polish characters

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ with dpg.texture_registry(show=False):
     dpg.add_static_texture(width=width, height=height, default_value=data, tag="iconpaste")
 
 def pobierz(sender, app_data):
-    print("link: ", dpg.get_value("textbox"))
-    print("sciezka: ", dpg.get_value("textbox2"))
     subprocess.run(["gallery-dl", dpg.get_value("textbox"), "-d", dpg.get_value("textbox2")])
 
 def filecallback(sender, app_data):
@@ -37,14 +35,22 @@ dpg.add_file_dialog(
 with dpg.item_handler_registry(tag="widget handler") as handler:
     dpg.add_item_clicked_handler(callback=pobierz)
 
+with dpg.font_registry():    
+    with dpg.font("Anonymous_Pro.ttf", 20) as font1:
+        dpg.add_font_chars([0x0105, 0x0104, 0x0107, 0x0106, 0x0119, 0x0118, 0x0142, 0x0141, 0x0144, 0x0143, 0x00f3, 0x00d3, 0x015b, 0x015b, 0x015a, 0x017a, 0x0179, 0x017c, 0x017b])
+        default_font = font1
+
 with dpg.window(width=500, height=300, label="GUI"):
     dpg.add_text("Podaj link do galerii", tag="text item")
-    dpg.add_input_text(hint="http://", tag="textbox", callback=validation)
-    dpg.add_text(default_value="", tag="validate")
-    dpg.add_image_button(label="Wklej", tag="przyciskwklej", width=15, height=15, texture_tag="iconpaste", callback=wklej)
+    with dpg.group(horizontal=True):
+        dpg.add_input_text(hint="http://", tag="textbox", callback=validation)
+        dpg.add_image_button(label="Wklej", tag="przyciskwklej", width=15, height=15, texture_tag="iconpaste", callback=wklej)
+        dpg.add_text(default_value="", tag="validate")    
+    dpg.add_button(label="Wybierz ścieżkę", callback=lambda: dpg.show_item("file_dialog_id"))
+    dpg.add_input_text(default_value=downloads_path, tag="textbox2")
+    dpg.add_text()
     dpg.add_button(label="Pobierz", tag="przyciskpobierz", callback=pobierz)
-    dpg.add_button(label="Wybierz sciezke", callback=lambda: dpg.show_item("file_dialog_id"))
-    dpg.add_input_text(hint=downloads_path, tag="textbox2")
+    dpg.bind_font(default_font)
 
 # bind item handler registry to item
 dpg.bind_item_handler_registry("przyciskpobierz", "widget handler")
